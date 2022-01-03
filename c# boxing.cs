8110defa4a1e46c7a18508a0cfb49fcc5de7de0b
krishn kant vashishtha <65147813+krishn139200@users.noreply.github.com>
2022-01-03 20:27:40 +0530

    1.1

---
+++ c# boxing.cs
@@ -0,0 +1,18 @@
+// C# implementation of Boxing
+using System;
+
+class program {
+
+	// Main Method
+	static public void Main()
+	{
+		// int value data type
+		int val = 8;
+
+		// boxing
+		object obj = val;
+
+		Console.WriteLine("val = {0}", val);
+		Console.WriteLine("obj = {0}", obj);
+	}
+}
